8dce9bf4dc89a4652e10a7065542e26fa0867cb9
defabdullah <ahmet.susuz@boun.edu.tr>
2022-05-05 01:17:42 +0300

    add print lbl to file but brok. (A,B and more).

--- first.asm
+++ first.asm
@@ -1,5 +1,10 @@
-jmp read_input    
-temp dw 0
+jmp start   
+temp dw 0h
+number db 5 dup 0h	; stores output string
+count dw 0h
+cr dw 10, 13, "$"
+
+
 number_finish:
 	mov ah, 0 ; make the top half zero 
 	push cx ; push number to stack
@@ -14,19 +19,24 @@ read_input:
 	cmp dl,20h ;check space
 	je number_finish
 	cmp dl, 0dh ; check enter
-	je exit
-	cmp dl, '+' ; check enter
+	je setup_string
+	cmp dl, '+' ; check plus sign
 	je add_them
-	cmp dl, '*' ; check enter
+	cmp dl, '*' ; check multiply sign
 	je mult_them
 	sub dx,'0' ; take actual value
 	mov temp,dx ; last digit will be added
-	mov cx,16 
+	mov cx,10h 
 	mul cx ; digit shift to left
 	add ax,temp ;add last digit
 	mov cx,ax ;hold current value
 	mov ax,0
 	jmp read_input ;loop
+
+start:
+	mov cx,0	 			; cx will hold the current integer
+	mov bl,0      			; bl will be used as counter
+	jmp read_input
 
 add_them:
 	mov ah, 0 ;
@@ -34,6 +44,7 @@ add_them:
 	pop dx  
 	add ax,dx  
 	push ax  ;take last 2 element and add them then push to stack
+	mov ax,0
 	jmp read_input ;continue to read
 
 mult_them:
@@ -42,8 +53,35 @@ mult_them:
 	pop dx
 	mul dx
 	push ax ;take last 2 element multiply  them then push to stack
+	mov ax,0
 	jmp read_input ;continue to read
 
+setup_string:
+	pop ax
+	mov bx,offset number+4 	; put a $ at end of buffer
+	mov b[bx],"$"			; we will fill buffer from back
+	dec bx
+
+convert_hexadecimal:
+	mov dx,0
+	mov cx,10h
+	div cx					; divide ax (i.e. current number) by 10 to get the last digit
+	add dx,48d  			; convert remainder (last digit) to its ASCII representation
+	mov [bx],dl				; and move to buffer for output
+	dec bx
+	inc count
+    cmp count,04h			; check if we have got all digits
+	jnz convert_hexadecimal
+print_cr:
+	mov ah,09
+	mov dx,offset cr		; print out a carriage return character
+	int 21h
+
+print_out:
+	mov dx,bx				; give the address of string to dx
+	inc dx					; we decremented once too many, go forward one
+	mov ah,09
+	int 21h
 exit:
 	mov ah, 04ch
 	mov al, 00
